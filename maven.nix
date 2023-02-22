636388383e5a234f855659bf5cb2bc44cb34b65e
Pavel Chuprikov <pschuprikov@gmail.com>
2023-02-22 14:18:39 +0100

    allow hash overrides

--- maven.nix
+++ maven.nix
@@ -7,19 +7,20 @@ with lib; rec {
   # 	mvn package --offline -Dmaven.repo.local=${repository}
   #
   # @param dependencies: A attrset of dependencies to build the repository
-  buildMavenRepository = { dependencies, drvDependencies ? [] }:
+  buildMavenRepository = { dependencies, drvDependencies ? [], hashOverrides ? {} }:
     let
       findDrvDependency = layout: findFirst 
           (d: d.layout == layout) 
             (abort "no url or derivation dependency for ${layout}") 
             drvDependencies;
-      dependenciesAsDrv = (forEach (attrValues dependencies) (dependency: {
+      dependencyAsDrv = name: dependency: {
         drv = if hasAttr "url" dependency then fetchurl {
           url = dependency.url;
-          sha256 = dependency.sha256;
+          sha256 = if hashOverrides ? ${name} then hashOverrides.${name} else dependency.sha256;
         } else (findDrvDependency dependency.layout).drv;
         layout = dependency.layout;
-      }));
+      };
+      dependenciesAsDrv = forEach (attrNames dependencies) (name: dependencyAsDrv name dependencies.${name});
     in linkFarm "mvn2nix-repository" (forEach dependenciesAsDrv (dependency: {
       name = dependency.layout;
       path = dependency.drv;
@@ -32,8 +33,8 @@ with lib; rec {
   # 	mvn package --offline -Dmaven.repo.local=${repository}
   #
   # @param file: A path to a file containing the JSON output of running mvn2nix
-  buildMavenRepositoryFromLockFile = { file, drvDependencies ? [] }:
+  buildMavenRepositoryFromLockFile = { file, drvDependencies ? [], hashOverrides ? {} }:
     let
       dependencies = (builtins.fromJSON (builtins.readFile file)).dependencies;
-    in buildMavenRepository { inherit dependencies drvDependencies; };
+    in buildMavenRepository { inherit dependencies drvDependencies hashOverrides; };
 }
